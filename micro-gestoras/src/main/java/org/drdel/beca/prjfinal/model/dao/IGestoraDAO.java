3d0c9e7b3f6101bbe82b4ad99e46ce7062c553c8
Oscar Yataco <oscarjorge97@gmail.com>
2022-12-30 02:28:29 +0100

    Añadiendo import.sql(t_gestora_fondos, t_p_inversion), package dao (IGestoraDAO, IPoliticaInversionDAO) y el package entity (Gestora, PoliticaInversion)

---
+++ micro-gestoras/src/main/java/org/drdel/beca/prjfinal/model/dao/IGestoraDAO.java
@@ -0,0 +1,7 @@
+package org.drdel.beca.prjfinal.model.dao;
+
+import org.drdel.beca.prjfinal.model.entity.Gestora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IGestoraDAO extends JpaRepository<Gestora,Long> {
+}
