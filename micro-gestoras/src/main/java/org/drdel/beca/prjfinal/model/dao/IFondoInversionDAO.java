1f77de08f568a460c2bebdcbbc9c61d8b7106088
Oscar Yataco <oscarjorge97@gmail.com>
2022-12-30 02:08:01 +0100

    Añadiendo import.sql (DireccionGestora y FondoInversion), package dao (IDireccionGestoraDAO, IFondoInversionDAO) y package entity (DireccionGestora, FondoInversion)

---
+++ micro-gestoras/src/main/java/org/drdel/beca/prjfinal/model/dao/IFondoInversionDAO.java
@@ -0,0 +1,7 @@
+package org.drdel.beca.prjfinal.model.dao;
+
+import org.drdel.beca.prjfinal.model.entity.FondoInversion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IFondoInversionDAO extends JpaRepository<FondoInversion,Long> {
+}
