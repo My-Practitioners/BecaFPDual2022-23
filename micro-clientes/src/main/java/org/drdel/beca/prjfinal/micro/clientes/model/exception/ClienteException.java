09b29c73b1b4059f7e9859c6bf6fdc9446883044
pesparis <pablo.esparis.cupido@accenture.com>
2023-01-17 11:28:30 +0100

    Creación ClienteException

--- micro-clientes/src/main/java/org/drdel/beca/prjfinal/micro/clientes/model/exception/ClienteException.java
+++ micro-clientes/src/main/java/org/drdel/beca/prjfinal/micro/clientes/model/exception/ClienteException.java
@@ -1,5 +1,33 @@
 package org.drdel.beca.prjfinal.micro.clientes.model.exception;
 
-public class ClienteException {
+public class ClienteException extends Exception{
 
+    public ClienteException(String mensaje){
+        super(mensaje);
+    }
+
+    public static void crearCliente() throws ClienteException{
+
+        throw new ClienteException("El estado del nuevo cliente debe ser DRAFT(4)");
+    }
+
+    public static void borrarCliente() throws ClienteException{
+
+        throw new ClienteException("Para borrar el cliente, su estado debe ser DRAFT(4)");
+    }
+
+    public static void activarCliente() throws ClienteException{
+
+        throw new ClienteException("Para activar un cliente, su estado debe ser DRAFT(4)");
+    }
+
+    public static void suspenderCliente() throws ClienteException{
+
+        throw new ClienteException("Para suspender un cliente, su estado debe ser OPERATIVE(1)");
+    }
+
+    public static void cancelarCliente() throws ClienteException{
+
+        throw new ClienteException("Para cancelar un cliente, su estado debe ser OPERATIVE(1)");
+    }
 }
