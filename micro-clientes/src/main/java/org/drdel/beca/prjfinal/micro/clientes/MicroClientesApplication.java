f036799e171c2885785cb7c28d1749b7284ab0d9
Oscar Yataco <oscarjorge97@gmail.com>
2023-01-18 11:18:09 +0100

    metodoCrearCliente con estado en draft ya funciona, correccion de schema.sql.

--- micro-clientes/src/main/java/org/drdel/beca/prjfinal/micro/clientes/MicroClientesApplication.java
+++ micro-clientes/src/main/java/org/drdel/beca/prjfinal/micro/clientes/MicroClientesApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
