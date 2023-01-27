68b7d3855f7f984bd36195288110d7d367a11eeb
pesparis <pablo.esparis.cupido@accenture.com>
2023-01-27 11:00:55 +0100

    Properties creación

--- beca-micro-operacion/src/main/java/org/drdel/beca/prjfinal/micro/operaciones/MicroOperacionesApplication.java
+++ beca-micro-operacion/src/main/java/org/drdel/beca/prjfinal/micro/operaciones/MicroOperacionesApplication.java
@@ -2,8 +2,10 @@ package org.drdel.beca.prjfinal.micro.operaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "org.drdel.beca.prjfinal.micro.operaciones", "org.drdel.beca.prjfinal.micro.operaciones.api" , "org.drdel.beca.prjfinal.micro.operaciones.configuration"})
 public class MicroOperacionesApplication {
 
 	public static void main(String[] args) {
