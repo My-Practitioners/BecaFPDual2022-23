0d38a934565f65ec25baefee8a040733b946d688
oscar_ytc <oscarjorge97@gmail.com>
2023-05-27 12:20:24 +0200

    Micro-gestora. Conexión con base de datos MySQLworkbench y arreglo de código

--- micro-gestoras/src/main/java/org/drdel/beca/prjfinal/micro/gestoras/model/dao/IDireccionGestoraDAO.java
+++ micro-gestoras/src/main/java/org/drdel/beca/prjfinal/micro/gestoras/model/dao/IDireccionGestoraDAO.java
@@ -2,9 +2,10 @@ package org.drdel.beca.prjfinal.micro.gestoras.model.dao;
 
 import org.drdel.beca.prjfinal.micro.gestoras.model.entity.DireccionGestora;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface IDireccionGestoraDAO extends JpaRepository<DireccionGestora,Long> {
 
     List<DireccionGestora> findByDireccion(String direccionGestora);
